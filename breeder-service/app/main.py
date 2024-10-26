c08e6ee869d8046d3762231c5228d9a52a7e19b1
tianqizhao-louis <louiszhao0517@gmail.com>
2024-10-26 16:27:20 -0400

    fix critical bug

--- breeder-service/app/main.py
+++ breeder-service/app/main.py
@@ -1,43 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.breeders import breeders
-from app.api.db import metadata, database, engine
+from app.api.db import metadata, database, engine, initialize_database, cleanup
 from app.api.middleware import LoggingMiddleware
 from contextlib import asynccontextmanager
-
-from sqlalchemy.exc import IntegrityError
-
-import os
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup code: connect to the database
-    await database.connect()
+    await initialize_database()
     yield
     # Shutdown code: disconnect from the database
-    await database.disconnect()
-
-if os.getenv("FASTAPI_ENV") == "production":
-    try:
-        metadata.create_all(engine)
-    except IntegrityError as e:
-        print(f"Database already initialized: {e}")
-else:
-    metadata.drop_all(engine)
-    metadata.create_all(engine)
-
-
-# # Check if the environment is set to production
-# if os.getenv("FASTAPI_ENV") == "production":
-#     try:
-#         metadata.drop_all(engine)  # Clears existing tables
-#         metadata.create_all(engine)  # Recreates tables
-#         print("Database tables dropped and recreated for production.")
-#     except IntegrityError as e:
-#         print(f"An error occurred during database initialization: {e}")
-# else:
-#     metadata.create_all(engine)
+    await cleanup()
 
 
 app = FastAPI(
