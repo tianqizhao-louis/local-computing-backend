3b69ff068ec120def9c5cbd0a0517efaaf524e42
tianqizhao-louis <louiszhao0517@gmail.com>
2024-10-26 15:42:47 -0400

    prod

--- breeder-service/app/main.py
+++ breeder-service/app/main.py
@@ -5,6 +5,10 @@ from app.api.db import metadata, database, engine
 from app.api.middleware import LoggingMiddleware
 from contextlib import asynccontextmanager
 
+from sqlalchemy.exc import IntegrityError
+
+import os
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,13 +18,32 @@ async def lifespan(app: FastAPI):
     # Shutdown code: disconnect from the database
     await database.disconnect()
 
+if os.getenv("FASTAPI_ENV") == "production":
+    try:
+        metadata.create_all(engine)
+    except IntegrityError as e:
+        print(f"Database already initialized: {e}")
+else:
+    metadata.drop_all(engine)
+    metadata.create_all(engine)
 
-metadata.create_all(engine)
+
+# # Check if the environment is set to production
+# if os.getenv("FASTAPI_ENV") == "production":
+#     try:
+#         metadata.drop_all(engine)  # Clears existing tables
+#         metadata.create_all(engine)  # Recreates tables
+#         print("Database tables dropped and recreated for production.")
+#     except IntegrityError as e:
+#         print(f"An error occurred during database initialization: {e}")
+# else:
+#     metadata.create_all(engine)
+
 
 app = FastAPI(
-    openapi_url="/api/v1/breeders/openapi.json", 
+    openapi_url="/api/v1/breeders/openapi.json",
     docs_url="/api/v1/breeders/docs",
-    lifespan=lifespan  # Use lifespan event handler
+    lifespan=lifespan,  # Use lifespan event handler
 )
 
 origins = [
